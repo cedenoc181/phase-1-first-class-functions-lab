aa18dae049c71eb4e15f1219ec8b9e2aec517041
Christian <cedenoc181@gmail.com>
2022-09-03 21:34:59 -0400

    commit tree 6b96f2e7bd736e19cab54a5e6ae763f4393c2b55

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+  function returnFirstTwoDrivers() {
+    return drivers.slice(0,2);
+    
+}
+console.log(returnFirstTwoDrivers());
+
+function returnLastTwoDrivers() {
+    return drivers.slice(2,4);
+    
+}
+console.log(returnLastTwoDrivers());
+
+const selectingDrivers = [returnFirstTwoDrivers(),returnLastTwoDrivers()];
+ console.log(selectingDrivers);
